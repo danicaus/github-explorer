63ea3265c860d979b786713c64c7514fd6ea8e17
Daniela Caus <danicaus.br@gmail.com>
2021-07-11 15:49:03 -0300

    Add RepositoryList component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
-import './styles/global.css';
+import { RepositoryList } from './components/RepositoryList';
+import './styles/global.scss';
 
 export function App() {
-    return <h1>Hello world!</h1>
+    return <RepositoryList/>
 }
---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,28 @@
+const repositoryName = 'Repositório 1'
+
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+            
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar repositório</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar repositório</a>
+                </li>
+            </ul>
+
+        </section>
+    )
+}
