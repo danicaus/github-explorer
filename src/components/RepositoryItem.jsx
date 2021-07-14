f32ab0205968747b5a67b303ce2a739caa76c07e
Daniela Caus <danicaus.br@gmail.com>
2021-07-13 22:37:29 -0300

    Insert interaction with Github API

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -3,7 +3,7 @@ export function RepositoryItem(props){
         <li>
             <strong>{props.repository.name}</strong>
             <p>{props.repository.description}</p>
-            <a href={props.repository.link}>Acessar repositório</a>
+            <a href={props.repository.html_url}>Acessar repositório</a>
         </li>
     )
 }
